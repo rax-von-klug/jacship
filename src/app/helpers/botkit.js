b52eb07b5f950aa24aa5339a77ce1651d81c401d
Christopher Lund <clund@connectionseducation.com>
2016-12-15 22:47:26 -0500

    fixed available command confirmation to display correct info

--- src/app/helpers/botkit.js
+++ src/app/helpers/botkit.js
@@ -143,7 +143,7 @@ export function join_shared_channel({ actions, team, channel }, callback) {
             controller.storage.shares.save(shared_channel);
 
             callback({ 
-                text: `:white_check_mark: You have joined the conversation in *${team.name}'s *#${channel.name}*!`,
+                text: `:white_check_mark: You have joined the conversation in *${shared_channel.team_name}'s* *#${shared_channel.channel_name}*!`,
                 replace_original: false 
             });
         });
